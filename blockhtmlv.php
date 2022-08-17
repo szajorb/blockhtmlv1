f1c765309ea8dadb24e5d993aad6d60f4b021929
Bartosz Szajor <78597883+szajorb@users.noreply.github.com>
2022-08-17 10:39:00 +0200

    Add files via upload

---
+++ blockhtmlv.php
@@ -0,0 +1,92 @@
+<?php
+/**
+* 2007-2022 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2022 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Blockhtmlv extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'blockhtmlv';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Bartek';
+        $this->need_instance = 0;
+
+       
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('blockhtmlv');
+        $this->description = $this->l('TPL z napisem <h3>hello world</h3> który będzie się wczydywał w dowolnym hooku skonfigurowany w panelu admina.');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+    public function install()
+    {
+        return parent::install() && $this->registerHook('header') && $this->registerHook('backOfficeHeader') && $this->registerHook('displayHeader');
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+    public function hookBackOfficeHeader()
+    {
+        $this->context->smarty->assign(
+            [
+                'content' => 'blokhtmlv',
+            ]
+        );
+    
+        return $this->display(__FILE__, 'blockhtmlv.tpl');
+    }
+    public function hookHeader()
+    {
+        $this->context->smarty->assign(
+            [
+                'content' => 'blokhtmlv',
+            ]
+        );
+    
+        return $this->display(__FILE__, 'blockhtmlv.tpl');
+    }
+    public function hookDisplayHeader()
+    {
+        $this->context->smarty->assign(
+            [
+                'content' => 'blokhtmlv',
+            ]
+        );
+    
+        return $this->display(__FILE__, 'blockhtmlv.tpl');
+    }
+}
